a1416064d8db9261b878e60b2e3a9d8337452277
sherif@shendidy.com <sherif@shendidy.com>
2020-05-29 00:48:43 +0100

    about toggle complete

---
+++ Assets/Scripts/Views/AboutToggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutToggle : MonoBehaviour
+{
+    public GameObject aboutPanel;
+
+    public void OpenAboutPanel()
+    {
+        if (aboutPanel != null) aboutPanel.SetActive(!aboutPanel.activeSelf);
+    }
+}

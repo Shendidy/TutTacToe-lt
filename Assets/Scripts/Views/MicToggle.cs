9bb6735afebd95839bc664cc67ec7d212655bb63
sherif@shendidy.com <sherif@shendidy.com>
2020-05-29 01:58:33 +0100

    implemented mute button

---
+++ Assets/Scripts/Views/MicToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicToggle : MonoBehaviour
+{
+    public GameObject micButton;
+    public GameObject muteButton;
+
+    public void micToggle()
+    {
+        if (micButton != null && muteButton != null)
+        {
+            micButton.SetActive(!micButton.activeSelf);
+            muteButton.SetActive(!muteButton.activeSelf);
+            AudioListener.pause = !AudioListener.pause;
+        }
+    }
+}

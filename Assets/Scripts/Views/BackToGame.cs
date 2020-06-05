26d0067d3d41810b8d36572b994441f0bc894be2
sherif@shendidy.com <sherif@shendidy.com>
2020-06-05 16:16:41 +0100

    cleanup

--- Assets/Scripts/Views/BackToGame.cs
+++ Assets/Scripts/Views/BackToGame.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.Rendering.PostProcessing; // useful??
+using UnityEngine.Rendering.PostProcessing;
 
 
 public class BackToGame : MonoBehaviour
@@ -10,19 +10,7 @@ public class BackToGame : MonoBehaviour
     public GameObject noKeysPanel;
     public GameObject fireworksPanel;
 
-
-
     public PostProcessVolume m_Volume;
-    Bloom m_Bloom;
-
-    public void Start()
-    {
-        //m_Bloom = ScriptableObject.CreateInstance<Bloom>();
-        //m_Bloom.enabled.Override(true);
-        //m_Bloom.intensity.Override(0f);
-
-        //m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Bloom);
-    }
 
     public void BackToGamePanel()
     {
@@ -32,13 +20,7 @@ public class BackToGame : MonoBehaviour
         if (noKeysPanel != null) noKeysPanel.SetActive(false);
         if (fireworksPanel != null) fireworksPanel.SetActive(false);
 
-        //m_Bloom = m_Volume.profile.settings[0];
-
-        //if (m_Volume.TryGetComponent(out m_Bloom))
-        //    m_Bloom.intensity.Override(0f);
-
         m_Volume.isGlobal = false;
-
     }
 }
 

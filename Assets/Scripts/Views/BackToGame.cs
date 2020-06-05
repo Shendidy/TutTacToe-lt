eb6852f66b321b2503a500cb83016520561ae163
sherif@shendidy.com <sherif@shendidy.com>
2020-06-05 16:15:13 +0100

    can modify bloom globality

--- Assets/Scripts/Views/BackToGame.cs
+++ Assets/Scripts/Views/BackToGame.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing; // useful??
+
 
 public class BackToGame : MonoBehaviour
 {
@@ -8,6 +10,20 @@ public class BackToGame : MonoBehaviour
     public GameObject noKeysPanel;
     public GameObject fireworksPanel;
 
+
+
+    public PostProcessVolume m_Volume;
+    Bloom m_Bloom;
+
+    public void Start()
+    {
+        //m_Bloom = ScriptableObject.CreateInstance<Bloom>();
+        //m_Bloom.enabled.Override(true);
+        //m_Bloom.intensity.Override(0f);
+
+        //m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Bloom);
+    }
+
     public void BackToGamePanel()
     {
         if (gameItemsPanel != null) gameItemsPanel.SetActive(true);
@@ -15,5 +31,19 @@ public class BackToGame : MonoBehaviour
         if (getKeysPanel != null) getKeysPanel.SetActive(false);
         if (noKeysPanel != null) noKeysPanel.SetActive(false);
         if (fireworksPanel != null) fireworksPanel.SetActive(false);
+
+        //m_Bloom = m_Volume.profile.settings[0];
+
+        //if (m_Volume.TryGetComponent(out m_Bloom))
+        //    m_Bloom.intensity.Override(0f);
+
+        m_Volume.isGlobal = false;
+
     }
 }
+
+
+
+
+
+

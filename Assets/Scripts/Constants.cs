0c958eb76ef123f73ea3c621b1ea5866944f31b0
sherif@shendidy.com <sherif@shendidy.com>
2020-06-06 03:06:45 +0100

    fixed new name

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,10 +6,13 @@ public class Constants
     public const int _MaximumDailyKeysRefill = 10;
     public const int _RefillOnRewardedAd = 5;
     public const string _RefillKeysString = "yyyy-MM-dd";
-    public const string _AdMobAppID = "ca-app-pub-6785760956809900~7030560283";
+    public const string _AdMobAppID = "ca-app-pub-6785760956809900~9030602312";
     public const string _AdMobBannerTestID = "ca-app-pub-3940256099942544/6300978111";
     public const string _AdMobInterstitialTestID = "ca-app-pub-3940256099942544/8691691433";
     public const string _AdMobRewardedTestID = "ca-app-pub-3940256099942544/5224354917";
+    public const string _AdMobBannerID = "ca-app-pub-6785760956809900/6620061397";
+    public const string _AdMobInterstitialID = "ca-app-pub-6785760956809900/2392987871";
+    public const string _AdMobRewardedID = "ca-app-pub-6785760956809900/2325593161";
     public const int _KeysPerBoard3x3 = 1;
     public const int _KeysPerBoard4x4 = 3;
     public const int _KeysPerBoard5x5 = 5;

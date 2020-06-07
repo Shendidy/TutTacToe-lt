0829becb1e1fefde1a2d04a60cfae836086735b0
sherif@shendidy.com <sherif@shendidy.com>
2020-06-07 02:24:07 +0100

    implemented difficulty toggle

---
+++ Assets/Scripts/Library/Actions/ToggleDifficulty.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleDifficulty : MonoBehaviour
+{
+    public GameObject easyButton;
+    public GameObject hardButton;
+
+    public void DifficultyToggle()
+    {
+        if (easyButton != null && hardButton != null)
+        {
+            easyButton.SetActive(!easyButton.activeSelf);
+            hardButton.SetActive(!hardButton.activeSelf);
+
+            GameManager.difficulty =
+                GameManager.difficulty == 1 ? 2 : 1;
+        }
+    }
+}

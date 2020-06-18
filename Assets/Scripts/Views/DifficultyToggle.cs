80129cf869a78b9f9a4474a80e2a975be6c46ca2
sherif@shendidy.com <sherif@shendidy.com>
2020-06-18 23:05:14 +0100

    can't change difficulty mid game

---
+++ Assets/Scripts/Views/DifficultyToggle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DifficultyToggle : MonoBehaviour
+{
+    public static DifficultyToggle instance;
+    public GameObject difficultyPanel;
+    public GameObject gameItemsPanel;
+
+    public void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void ToggleDifficultyPanel()
+    {
+        if (difficultyPanel != null) difficultyPanel.SetActive(!difficultyPanel.activeSelf);
+        if (gameItemsPanel != null) gameItemsPanel.SetActive(!gameItemsPanel.activeSelf);
+    }
+}
